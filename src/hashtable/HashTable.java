58622911d7161a34574837e6107c12b1901359b3
Tony <tony.g.thompson@gmail.com>
2018-03-11 20:23:11 -0700

    structure added

---
+++ src/hashtable/HashTable.java
@@ -0,0 +1,63 @@
+/**
+* Tony Thompson
+* Mar 11, 2018
+* HashTable.java
+* 
+*/
+package hashtable;
+
+import java.util.Iterator;
+
+import structures.ICollection;
+
+public class HashTable<T> implements ICollection<T> {
+
+	@Override
+	public void add(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(T element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(T element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
